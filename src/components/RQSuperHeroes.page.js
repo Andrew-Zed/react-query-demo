d548351199e25462c14c6e29c22ce06506c6cc21
Andrew-Zed <andrew.awadyke@gmail.com>
2022-05-13 00:47:09 +0100

    Queery by Id implemented

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { useSuperHeroesData } from '../hooks/useSuperHeroesData';
 
 
@@ -28,12 +29,16 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch Heroes</button>
-      {/* {data?.data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>;
-      })} */}
-      {data.map((heroName) => {
-        return <div key={heroName}>{heroName}</div>
+      {data?.data.map((hero) => {
+        return(
+          <div key={hero.id}>
+              <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        )
       })}
+      {/* {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>
+      })} */}
     </>
   );
 };
